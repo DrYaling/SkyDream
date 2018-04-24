2b7b2eb01a1bdaaef3eb4e646b7c150c8f362fda
zxb_game9z <zxb870219@163.com>
2018-04-24 18:12:41 +0800

    multi client and connection

--- SkyDream/SkyDream/Main.cpp
+++ SkyDream/SkyDream/Main.cpp
@@ -6,26 +6,35 @@
 #include <Socket/WorldSocket.h>
 #include <Asio/IoContext.h>
 #include "Common//Common.h"
-#include "Socket/WorldSocket.h"
 #include "Socket/Client.h"
 #include "Socket/AsyncAcceptor.h"
+#include "Socket/WorldSocketMgr.h"
 
 static void OnSocketAccept(tcp::socket* sock)
 {
 	//sWorldSocketMgr.OnSocketOpen(std::forward<tcp::socket>(sock), threadIndex);
 	std::cout << "SkyDream accept callback " << sock->remote_endpoint() << std::endl;
+	WorldSocket* conn = new WorldSocket(sock);
+	conn->Start();
+	sWorldSocketMgr->OnSocketConnect(conn);
 }
 void ThreadAccepter()
 {
-	tcp::socket * sock = sSocketMgr->GetSocket();
-	AsyncAcceptor* _acceptor = new AsyncAcceptor(sock, "0.0.0.0", 8080);
+	//创建服务器
+	tcp::socket * sock = sWorldSocketMgr->GetSocket();
+	AsyncAcceptor* _acceptor = new AsyncAcceptor(sock, "0.0.0.0", 8081);
 	if (!_acceptor->Bind())
 		std::cout << "error acceptor bind fail" << std::endl;
 	_acceptor->AsyncAcceptWithCallback<&OnSocketAccept>();
-	auto client = ClientSocket::Create();
-	Sleep(500);
-	client->Start("127.0.0.1", 8080);
-	while (1) Sleep(1);
+	//auto client = new  ClientSocket(sock);
+	//Sleep(500);
+	//client->Start("127.0.0.1", 8081);
+	while (1)
+	{
+		sWorldSocketMgr->Update();
+		Sleep(6);
+	}
+
 	delete _acceptor;
 
 }
